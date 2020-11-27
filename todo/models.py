a2abbe70c384a066bbb5eeae634c70c4ff7c3ada
RifatRobin <rifatitna@gmail.com>
2020-11-28 00:06:56 +0600

    created forms.py & TodoForm is working in the index file with showing the data of the model in the html page

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TodoList(models.Model):
+    work = models.CharField(max_length=200)
+    creation_Time = models.DateTimeField(auto_now_add=True)
+    completion_Status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.work
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import TodoList
+from .forms import *
 # Create your views here.
+
+
+def index(request):
+    td = TodoList.objects.all()
+    form = TodoForm()
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {'td': td, 'form': form}
+    return render(request, 'Todo/index.html', context)
