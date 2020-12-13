3dd318e4dfc12174ab6a308c2bab4ba4eab1fcfc
RifatRobin <rifatitna@gmail.com>
2020-12-13 19:15:49 +0600

    registration & login path created

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('edit/<str:pk>/', views.edit, name="edit"),
     path('delete/<str:pk>/', views.delete, name="delete"),
+    path('reg', views.registration, name="registration"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
 ]
